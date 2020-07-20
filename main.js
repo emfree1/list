8b35d927ca003a22dbcd6cbd0170c370335b05ed
emfree1 <emfree20@gmail.com>
2020-07-20 18:53:58 +0300

    add work

--- main.js
+++ main.js
@@ -1,55 +1,75 @@
 const todoList = document.querySelector(".todo-list");
-const deleteList = document.querySelector('.delete')
+const deleteList = document.querySelector(".delete");
 const inputList = document.querySelector(".add-list");
 const addButton = document.querySelector(".add-button");
 const todoItem = document.querySelector(".todo-item");
 
-function createNewElem(task) {
+const arrOld = [];
+const showData = (arr) => {
+  let newArr = [...arr];
   const inputList = document.createElement("li");
-  inputList.className = "todo-item";
+  newArr.map((item) => {
+    inputList.classList.add("todo-item");
+    inputList.innerHTML = `
+        <input class='checkbox' type='checkBox'>
+        <label class='title'>${item.task}</label>
+        <input class='textfield' type='text'>
+        `;
 
-  const checkBox = document.createElement("input");
-  checkBox.className = "checkbox";
-  checkBox.type = "checkBox";
-
-  const title = document.createElement("label");
-  title.className = "title";
-  title.innerHTML = task;
-
-  const textField = document.createElement("input");
-  textField.className = "textfield";
-  textField.type = "text";
-
-  const buttonEdit = document.createElement("button");
-  buttonEdit.className = "add";
-  buttonEdit.innerHTML='Змінити';
-
-  const buttonDelete = document.createElement("button");
-  buttonDelete.className = "delete";
-  buttonDelete.innerHTML='Видалити';
-
-  inputList.appendChild(checkBox);
-  inputList.appendChild(title);
-  inputList.appendChild(textField);
-  inputList.appendChild(buttonEdit);
-  inputList.appendChild(buttonDelete);
-
- 
-  return inputList
-}
-
-addButton.addEventListener('click', (event)=> {
-    event.preventDefault()
-    const inputValue = inputList.value
-
-    if(inputValue === '') {
-        alert('Введіть задачу')
-    } else {
-        const items = createNewElem(inputValue)
-        todoList.appendChild(items)
-        inputList.value = ''
-        
-    }
+    // const removeBtn = document.createElement("button");
+    // removeBtn.classList.add("add");
+    // removeBtn.textContent = "Змінити";
   
-})
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("add");
+    editBtn.textContent = "Змінити";
 
+    const toggleInput= () => {
+      const label = document.querySelector('.title')
+      const input = document.querySelector('.textfield')
+      const edit = inputList.classList.toggle('editing')
+
+      if (edit) {
+        input.value = label.innerHTML
+        editBtn.textContent = 'Зберегти'
+      } else {
+        label.innerHTML = input.value
+      }
+    }
+
+    editBtn.addEventListener('click', toggleInput)
+    
+
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("delete");
+    removeBtn.textContent = "Видалити";
+    removeBtn.addEventListener("click", () => {
+      inputList.remove();
+    });
+    inputList.append(editBtn)
+    inputList.append(removeBtn);
+    todoList.append(inputList);
+    
+  });
+
+
+};
+
+
+
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const inputValue = inputList.value;
+
+  if (inputValue === "") {
+    alert("Введіть задачу");
+  } else {
+    const formData = {
+      id: Math.floor(Math.random() * 1000),
+      task: inputValue,
+    };
+    arrOld.push(formData);
+    showData(arrOld);
+    inputList.value = "";
+  }
+})
